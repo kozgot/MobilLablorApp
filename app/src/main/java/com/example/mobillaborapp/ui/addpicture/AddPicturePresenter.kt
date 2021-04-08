5ce40ccaebca160f6b8c9adbce01d6e578a28d85
kozgot <gogota97@gmail.com>
2021-04-08 21:19:21 +0200

    Add package for new picture screen

---
+++ app/src/main/java/com/example/mobillaborapp/ui/addpicture/AddPicturePresenter.kt
@@ -0,0 +1,7 @@
+package com.example.mobillaborapp.ui.addpicture
+
+import com.example.mobillaborapp.ui.Presenter
+
+object AddPicturePresenter : Presenter<AddPictureScreen?>() {
+    // todo: implementation
+}
