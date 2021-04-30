5347156e0f172afbab2a9bc5336751c3c58ac23d
kozgot <gogota97@gmail.com>
2021-04-30 15:01:40 +0200

    Add spinner WIP

--- app/src/main/java/com/example/mobillaborapp/ui/addpicture/AddPictureActivity.kt
+++ app/src/main/java/com/example/mobillaborapp/ui/addpicture/AddPictureActivity.kt
@@ -1,12 +1,49 @@
 package com.example.mobillaborapp.ui.addpicture
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.*
 import com.example.mobillaborapp.R
+import kotlinx.android.synthetic.main.activity_add_picture.*
 
-class AddPictureActivity : AppCompatActivity() {
+class AddPictureActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
+    var languages = arrayOf("Java", "PHP", "Kotlin", "Javascript", "Python", "Swift")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_picture)
+
+        var aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        with(mySpinner)
+        {
+            adapter = aa
+            setSelection(0, false)
+            onItemSelectedListener = this@AddPictureActivity
+            prompt = "Select a breed"
+            gravity = Gravity.CENTER
+
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        showToast(message = "Nothing selected")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        when (view?.id) {
+            1 -> showToast(message = "Spinner 2 Position:${position} and language: ${languages[position]}")
+            else -> {
+                showToast(message = "Spinner 1 Position:${position} and language: ${languages[position]}")
+            }
+        }
+    }
+
+    private fun showToast(context: Context = applicationContext, message: String, duration: Int = Toast.LENGTH_LONG) {
+        Toast.makeText(context, message, duration).show()
     }
 }
