a770bc5fa0a46657d442c2c49c64cbcd999ee452
kozgot <gogota97@gmail.com>
2021-05-02 19:29:34 +0200

    Coroutine testing

--- app/src/test/java/com/example/mobillaborapp/test/AddPictureTest.kt
+++ app/src/test/java/com/example/mobillaborapp/test/AddPictureTest.kt
@@ -1,14 +1,17 @@
 package com.example.mobillaborapp.test
 
+import com.example.mobillaborapp.model.database.DbBreed
 import com.example.mobillaborapp.model.network.Breed
 import com.example.mobillaborapp.testInjector
 import com.example.mobillaborapp.ui.addpicture.AddPicturePresenter
 import com.example.mobillaborapp.ui.addpicture.AddPictureScreen
 import com.example.mobillaborapp.utils.argumentCaptor
 import com.example.mobillaborapp.utils.mock
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
-
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
@@ -22,22 +25,31 @@ class AddPictureTest {
 
     private lateinit var addPictureScreen: AddPictureScreen
 
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
     @Before
     @Throws(Exception::class)
     fun setup() {
         testInjector.inject(this)
         addPictureScreen = mock()
         addPicturePresenter.attachScreen(addPictureScreen)
+        Dispatchers.setMain(mainThreadSurrogate)
     }
 
     @Test
-    fun testLoadImage() {
-        addPicturePresenter.loadImagesFromAPI()
+    fun testQueryBreedsFromDB() {
+        runBlocking {
+            launch(Dispatchers.Main) {  // Will be launched in the mainThreadSurrogate dispatcher
+                val list: List<DbBreed> =
+                        withContext(Dispatchers.IO) {
+                            addPicturePresenter.queryBreedsFromDb()
+                        }
 
-        val list = argumentCaptor<MutableList<Image>>()
-        Mockito.verify(addPictureScreen).showImages(list.capture())
-        assert(list.value.size > 0)
+                assert(list.isNotEmpty())
+            }
+        }
     }
+
 
     @Test
     fun testLoadBreedsFromAPI() {
@@ -51,6 +63,8 @@ class AddPictureTest {
     @After
     fun tearDown() {
         addPicturePresenter.detachScreen()
+        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
     }
 
 }
